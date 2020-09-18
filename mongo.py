45e8669b1dc83b542601e8eed0fbf2809bfc4333
joshitft <joshi.suresh@tftus.com>
2020-09-18 15:43:08 +0530

    database connection

--- mongo.py
+++ mongo.py
@@ -7,8 +7,9 @@ from sklearn.metrics.pairwise import cosine_similarity
 app = flask.Flask(__name__)
 app.config["DEBUG"] = True
 
-client = MongoClient(
-    'mongodb+srv://binge_user:j5zK0ITpZzbbRkbR@bingedb.srj5f.gcp.mongodb.net/bingedb?retryWrites=true&w=majority')
+# client = MongoClient('mongodb+srv://binge_user:j5zK0ITpZzbbRkbR@bingedb.srj5f.gcp.mongodb.net/bingedb?retryWrites=true&w=majority')
+
+client = MongoClient('mongodb://admin:admin@localhost:5151/bingedb?authSource=admin')  # @TODO
 
 db = client.bingedb
 
@@ -39,7 +40,7 @@ def get_movie_by_index(index, movies):
 def home():
     movies_model = db.movies
 
-    get_all_movies = movies_model.find()[0:150]
+    get_all_movies = movies_model.find({}, {'title': 1, 'release_date': 1, 'adult': 1, 'genre_ids': 1})[0:150]
 
     combined_features = []
     movie_by_index = []
@@ -62,15 +63,15 @@ def home():
 
     sorted_similar_movies = sorted(similar_movies, key=lambda x: x[1], reverse=True)[1:]
 
-    top_five_movies = []
+    top_fifty_movies = []
 
     i = 0
     for element in sorted_similar_movies:
-        top_five_movies.append(get_movie_by_index(element[0], movie_by_index))
+        top_fifty_movies.append(get_movie_by_index(element[0], movie_by_index))
         i = i + 1
-        if i >= 50:
+        if i >= 10:
             break
-    return jsonify(top_five_movies)
+    return jsonify(top_fifty_movies)
 
 
 @app.route('/')
